7fc836044ad0a9852c3cc0ee7a55379b18f5d043
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2012-01-15 16:46:32 +0100

    now computes texture segmentation

--- examples/chinese_temple.py
+++ examples/chinese_temple.py
@@ -12,9 +12,30 @@ mem = Memory(cachedir='.')
 image = data.chinese()
 
 for k in [0.005, 0.05, 0.01, 0.1, 0.5]:
-    for hs in [0.1, 0.25, 0.5, 1, 2, 5]:
+    for hs in [0.1]:
         for hr in [1]:
             im = image.copy()
+            for num_bins in range(16, 128, 40):
+                desc = mem.cache(descriptors.extract_descriptors)(
+                                        im,
+                                        use_hog=False,
+                                        verbose=False, num_bins=num_bins)
+                descs = desc.astype(float)
+                #descs[:, :-5] = normalize(descs[:, :-5])
+                #descs[:, -5:-2] = normalize(descs[:, -5:-2])
+                ms = mem.cache(cluster.meanshift)(descs, k, hs=hs, hr=hr)
+                labels = ms.labels_
+                num = np.sqrt(len(labels))
+                results = labels.reshape((num, num)).T
+
+                imsave('images/lena_texture%d_meanshift_%d_%d_%d.png' % (
+                                                                    num_bins,
+                                                                    k * 100,
+                                                                    hs,
+                                                                    hr),
+                    results)
+
+
             print 'computing for cluster %d' % k
             print 'RGB'
             im = image.copy()
